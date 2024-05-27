10249cee73355a163059b74e7c37e746bbacbb43
lr580 <2277379149@qq.com>
2024-05-28 00:22:29 +0800

    fix

--- .umirc.ts
+++ .umirc.ts
@@ -2,7 +2,8 @@ import { defineConfig } from "umi";
 
 export default defineConfig({
   base: "/docs",
-  publicPath: "/docs/",
+  // publicPath: "/docs/", // 本地
+  publicPath: "https://lr580.github.io/typeCGame/docs/", // github page
   title: "打字练习",
   outputPath: 'docs',
   routes: [
