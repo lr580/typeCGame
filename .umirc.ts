8695b908a3e7e3b452e26e12b9d645dafc4ac507
lr580 <2277379149@qq.com>
2024-05-28 01:27:28 +0800

    101010

--- .umirc.ts
+++ .umirc.ts
@@ -3,8 +3,8 @@ import { defineConfig } from "umi";
 export default defineConfig({
   // base: "/docs", // 本地
   // publicPath: "/docs/", // 本地
-  base: "/codeCGame", // githubpages
-  publicPath: "/codeCGame/", // githubpages
+  base: "/typeCGame", // githubpages
+  publicPath: "/typeCGame/", // githubpages
   title: "打字练习",
   outputPath: 'docs',
   routes: [
